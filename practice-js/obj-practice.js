0ff34dea2a5a63ed87f1dc60836eb0d77c4849d7
shogo080 <258662@st.takushoku-u.ac.jp>
2026-06-18 16:44:43 +0900

    演習3-6

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,10 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
-
+console.log(campus.address);
+for(let n of campus.buildingD){
+  console.log(n);
+}
+for (let n of gakka){
+  console.log(n.name);
+}
